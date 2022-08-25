e4c60628ce6d77b702a66ec28879eb59927c881b
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-08-25 19:20:28 +0600

    Test with TIMEOUT parameter

---
+++ src/main/java/NetworkUtils.java
@@ -0,0 +1,8 @@
+public class NetworkUtils {
+
+    public static void getConnection() throws InterruptedException {
+        // connection with a server
+//        Thread.sleep(1010);
+        return;
+    }
+}
